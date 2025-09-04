e29c90f4a6c16da9c04f3c38cad4b531073c7cab
Rajina <rajinaneupane251@gmail.com>
2025-09-04 09:40:42 +0545

    assignment complete 2

--- src/q11.c
+++ src/q11.c
@@ -3,3 +3,20 @@
      ***
       *
 */
+#include <stdio.h>
+int main() {
+    int i, j, k;
+    int n = 3; // Number of rows for the upper half of the pattern
+    for(i = n; i >= 1; i--) { // Loop for each row
+        // Print leading spaces
+        for(k = n; k > i; k--) {
+            printf(" ");
+        }
+        // Print asterisks
+        for(j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n"); // Move to the next line after each row
+    }
+    return 0;
+}
